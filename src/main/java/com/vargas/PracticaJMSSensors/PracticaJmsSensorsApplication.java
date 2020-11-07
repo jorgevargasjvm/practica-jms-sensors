08dd07fb63d3d0de69e3273b069c6955736b2bf6
Jorge Vargas <jorgevargasmateo@hotmail.com>
2020-11-07 16:34:57 -0400

    initial commits

---
+++ src/main/java/com/vargas/PracticaJMSSensors/PracticaJmsSensorsApplication.java
@@ -0,0 +1,13 @@
+package com.vargas.PracticaJMSSensors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaJmsSensorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaJmsSensorsApplication.class, args);
+	}
+
+}
